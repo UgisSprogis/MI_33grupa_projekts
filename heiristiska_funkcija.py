91560bfb45abf3c6fcedb3639773100addae8895
Sandijs Silis <sandijs.silis@edu.rtu.lv>
2024-03-18 00:16:47 +0200

    commit12

--- heiristiska_funkcija.py
+++ heiristiska_funkcija.py
@@ -6,6 +6,7 @@ def heiristiska_funckija(sak_virsotne,beigu_virsotne,sak_seciba):
     celi = []
     uzvarosais_cels = []
     next_inline = []
+    next_inline_temp = []
     print("heiristiska_funckija")
     for x in sp.loki:
         for y in sp.loki[x]:
@@ -22,8 +23,12 @@ def heiristiska_funckija(sak_virsotne,beigu_virsotne,sak_seciba):
         for x in next_inline:
             for y in celi:
                 if y[1] == x:
-                    next_inline.append(y[0])
+                    next_inline_temp.append(y[0])
+                if y[0] == sak_virsotne:
+                    loop = False
+                    next_inline = next_inline_temp
+    print("Next inline: " + str(next_inline))
     
     
 
-heiristiska_funckija("A1", uzvaras_strupceli(sakums), sakums)
+heiristiska_funckija('A1', uzvaras_strupceli(sakums), sakums)
