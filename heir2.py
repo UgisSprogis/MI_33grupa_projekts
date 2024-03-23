86cfdb59dfa5447911b669d667d72813896f944e
Ugis <ugisuu@gmail.com>
2024-03-23 10:13:01 +0200

    Add minimax algorithm implementation  and commented all functions

--- heir2.py
+++ heir2.py
@@ -1,11 +1,12 @@
 from heiristiska_funkcija import *
 
-
+#Tiek noteikts maksimālais līmenis
 max_limenis = sp.virsotnes[len(sp.virsotnes)-1].limenis
+#Tiek izveidots saraksts ar visām virsotnēm
 atk_virsotnes=[]
 for x in sp.virsotnes:
     atk_virsotnes.append([x.id,x.skaitlis,x.speletajs1,x.speletajs2,x.limenis])
-
+#Tiek izveidota funkcija, kas atgriež visus bērnus no konkrētas virsotnes
 def berni(pasreizeja_virsotne):
     berni = []
     a = []
@@ -16,16 +17,28 @@ def berni(pasreizeja_virsotne):
                 berni.append(y)
     return berni
 
-
+#Tiek izveidota minimax funkcija, kā ievades parametrs ir virsotnes objekts, vai arī saraksts ar virsotnes objektiem
+# Funkcijas paraugs ir ņemts pēc grāmatas piemēra
+#Coppin B. (2004). Artificial Intelligence Illuminated. Jones and Bartlett Publishers.
+# 150lpp.
 def minimax(virs):
+    #Ja virsotne ir gala līmenī, tad tiek atgriezta novērtējuma vērtība
     if virs[4] == max_limenis:
         return virs[4] + virs[2] - virs[3]
-    next_states = berni(virs)
-    if not isinstance(next_states, list):
-        next_states = [next_states]
+    #Ja virsotne nav gala līmenī, tad tiek izveidots saraksts ar visiem bērniem
+    nakosais_stavoklis = berni(virs)
+    #if not isinstance(nakosais_stavoklis, list): pārbauda vai ir saraksts
+    #Ja nav saraksts, tad tiek izveidots saraksts
+    if not isinstance(nakosais_stavoklis, list):
+        nakosais_stavoklis = [nakosais_stavoklis]
+    #Ja virsotnes līnenis ir pāra skaitlis, tad tiek izvēlēts minimizēšanas algoritms no minimax funkcijas
     if (virs[4] % 2) == 0:
-        return min(minimax(state) for state in next_states)
+        #Tiek rekursīvi atgriezta minimālā vērtība no visiem bērniem
+        return min(minimax(stavoklis) for stavoklis in nakosais_stavoklis)
+    #Ja virsotnes līnenis ir nepāra skaitlis, tad tiek izvēlēts maksimizēšanas algoritms no minimax funkcijas
     elif (virs[4] % 2) == 1:
-        return max(minimax(state) for state in next_states)
+        #Tiek rekursīvi atgriezta maksimālā vērtība no visiem bērniem
+        return max(minimax(stavoklis) for stavoklis in nakosais_stavoklis)
 
+#Tiek izprintēta minimax funkcijas atgrieztā vērtība
 print(minimax(atk_virsotnes[0]))
